1025744ae5aca0c38cbcf53e9b1f0113dafb28b2
m2gen <ymslash78@gmail.com>
2024-04-21 18:03:38 +0900

    post right close cording

--- real-sns-frontend/src/components/post/Post.jsx
+++ real-sns-frontend/src/components/post/Post.jsx
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Post.css";
 import { MoreVert } from '@mui/icons-material';
+import { Users } from "../../../src/dummyData";
 
-export default function Post() {
+
+export default function Post({ post }) {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  }
+
   return (
     <div className='post'>
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img src="./assets/person/1.jpeg" alt="" className='postProfileImg' />
-            <span className="postUsername">ユーザーネム夫</span>
-            <span className="postdate">5分前</span>
+            <img src={Users.filter((user) => user.id === post.id)[0].profilePicture}
+              alt="" className='postProfileImg' />
+            <span className="postUsername">
+              {Users.filter((user) => user.id === post.id)[0].username}
+            </span>
+            <span className="postdate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">SNSを作ってます</span>
-          <img src="./assets/post/1.jpeg" alt="" className='postImg' />
+          <span className="postText">{post.desc}</span>
+          <img src={post.photo} alt="" className='postImg' />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img src="./assets/heart.png" alt="" className='likeIcon' />
-            <span className="postLikeCounter">5人がいいねを押しました</span>
+            <img src="./assets/heart.png" alt="" className='likeIcon' onClick={() => handleLike()} />
+            <span className="postLikeCounter">{like}人がいいねを押しました</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">4:コメント</span>
+            <span className="postCommentText">{post.comment}:コメント</span>
           </div>
         </div>
       </div>
--- real-sns-frontend/src/components/sidebar/Sidebar.jsx
+++ real-sns-frontend/src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,9 @@
 import { Bookmark, Home, MessageRounded, Notifications, Person, Search, Settings } from '@mui/icons-material'
 import React from 'react'
 import "./Sidebar.css";
+import CloseFriend from '../closeFriend/CloseFriend';
+import { Users } from "../../../src/dummyData";
+
 
 function Sidebar() {
   return (
@@ -39,18 +42,9 @@ function Sidebar() {
           </ul>
           <hr className="sidebarHr" />
           <ul className="sidebarFriendList">
-            <li className="sidebarFriend">
-              <img src="/assets/person/2.jpeg" alt="" className='sidebarFriendImg' />
-              <span className="sidebarFriendName">かんたくん</span>
-            </li>
-            <li className="sidebarFriend">
-              <img src="/assets/person/3.jpeg" alt="" className='sidebarFriendImg' />
-              <span className="sidebarFriendName">shinくん</span>
-            </li>
-            <li className="sidebarFriend">
-              <img src="/assets/person/4.jpeg" alt="" className='sidebarFriendImg' />
-              <span className="sidebarFriendName">けんけん</span>
-            </li>
+            {Users.map((user) => (
+              <CloseFriend user={user} key={user.id} />
+            ))}
           </ul>
         </div>
       </div>
