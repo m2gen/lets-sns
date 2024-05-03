704e5510150447304c996ecc3b4e2bda9df187af
m2gen <ymslash78@gmail.com>
2024-05-03 22:01:53 +0900

    Com

--- frontend/src/state/AuthContext.js
+++ frontend/src/state/AuthContext.js
@@ -1,22 +1,22 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 // 最初のユーザー状態を定義
 const initialState = {
-  // user: null,
-  user: {
-    _id: "6618041935569bc65c3c71ef",
-    username: "pan",
-    email: "shincode@gmail.com",
-    password: "122121212121",
-    profilePicture: "/person/1.jpeg",
-    coverPicture: "",
-    followers: [],
-    followings: [],
-    isAdmin: false,
-  },
-  isFetching: false,
-  error: false,
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  //   user: {
+  //     _id: "6618041935569bc65c3c71ef",
+  //     username: "pan",
+  //     email: "shincode@gmail.com",
+  //     password: "122121212121",
+  //     profilePicture: "/person/1.jpeg",
+  //     coverPicture: "",
+  //     followers: [],
+  //     followings: [],
+  //     isAdmin: false,
+  //   },
+  //   isFetching: false,
+  //   error: false,
 };
 
 // 状態をグローバルに管理する
@@ -24,6 +24,11 @@ export const AuthContext = createContext(initialState);
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user))
+  }, [state.user]);
+
   return (
     <AuthContext.Provider
       value={{
